1774ef9325d0097b877354501c3128c9650c55b3
atchapcyp <atcha.crypto@gmail.com>
2019-03-18 12:30:14 +0700

    before rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'courses#index', as: 'home'
+  
 
   get 'mycourse' => 'mycourses#mycourses',as: 'mycourse'
   resources :courses
+  root 'courses#index', as: 'home'
 end
